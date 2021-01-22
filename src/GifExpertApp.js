89ae0efbabf74963044fe27b62f7293d24d48f07
Jose Armando JALC. Lara Canchola <armando.lara@jalisco.gob.mx>
2021-01-22 16:39:26 -0600

    First Commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,38 @@
+//rafc
+import React, { Fragment, useState } from "react"
+import { AddCategory } from "./components/AddCategory"
+import { GifGrid } from "./components/GifGrid"
+
+
+const GifExpertApp = () => {
+    //const categories = ['One Punch', 'Samurai X', 'Dragon Ball'];
+    const [categories, setCategories] = useState(['One Punch'])
+
+    /*const handleAdd = () => {
+        setCategories([ ...categories, 'HunterXHunter']);
+        //setCategories([ 'HunterXHunter', ...categories]);
+        //setCategories( cats => [...cats, 'HunterXHunter']);
+    }*/
+
+    return(
+        <Fragment>
+            <h2>GifExpertApp</h2>
+            <AddCategory setCategories={ setCategories }/>
+            <hr/>
+
+
+            <ol>
+                {
+                    categories.map( category  => (
+                        <GifGrid 
+                            key={ category }
+                            category={ category }
+                        />
+                    ))
+                }
+            </ol>
+        </Fragment>
+    )
+}
+
+export default GifExpertApp;
